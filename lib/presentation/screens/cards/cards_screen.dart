c20a723779d569024d96eb4defe7b6bbb9ae34f3
Santiago Millan <santiagomillanpardo@gmail.com>
2024-03-17 13:53:32 -0500

    Cards finish, cards con img

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -45,6 +45,17 @@ class _CardsView extends StatelessWidget {
                 elevation: card['elevation'],
                 label: card['label'],
               )),
+
+          ...cards.map((card) => _CardType3(
+                elevation: card['elevation'],
+                label: card['label'],
+              )),
+
+          ...cards.map((card) => _CardType4(
+                elevation: card['elevation'],
+                label: card['label'],
+              )),
+          const SizedBox(height: 50),
         ],
       ),
     );
@@ -112,6 +123,79 @@ class _CardType2 extends StatelessWidget {
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _CardType3 extends StatelessWidget {
+  final String label;
+  final double elevation;
+  const _CardType3({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Card(
+      color: colors.surfaceVariant,
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: const Icon(Icons.more_vert_outlined),
+                onPressed: () {},
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('$label - Filled'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType4 extends StatelessWidget {
+  final String label;
+  final double elevation;
+  const _CardType4({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    // final colors = Theme.of(context).colorScheme;
+    return Card(
+      // color: colors.surfaceVariant,
+      clipBehavior: Clip.hardEdge,
+      elevation: elevation,
+      child: Stack(
+        children: [
+          Image.network(
+            'https://picsum.photos/id/${elevation.toInt()}/600/250',
+            height: 350,
+            fit: BoxFit.cover,
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                ),
+              ),
+              child: IconButton(
+                icon: const Icon(Icons.more_vert_outlined),
+                onPressed: () {},
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
