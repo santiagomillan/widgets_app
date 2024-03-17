ddd3ab470110875f7ef1ab8b772bc5cbd0f77d20
Santiago Millan <santiagomillanpardo@gmail.com>
2024-03-17 02:52:13 -0500

    _CardType2, border

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -41,6 +41,10 @@ class _CardsView extends StatelessWidget {
           //       elevation: card['elevation'],
           //       label: card['label'],
           //     )),
+          ...cards.map((card) => _CardType2(
+                elevation: card['elevation'],
+                label: card['label'],
+              )),
         ],
       ),
     );
@@ -70,6 +74,41 @@ class _CardType1 extends StatelessWidget {
             Align(
               alignment: Alignment.bottomLeft,
               child: Text(label),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType2 extends StatelessWidget {
+  final String label;
+  final double elevation;
+  const _CardType2({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Card(
+      shape: RoundedRectangleBorder(
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+          side: BorderSide(color: colors.outline)),
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: const Icon(Icons.more_vert_outlined),
+                onPressed: () {},
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('$label - outline'),
             )
           ],
         ),
