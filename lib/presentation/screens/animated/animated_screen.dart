678405d1ef0756477d4f3cf21faf05244a77fce5
Santiago Millan <santiagomillanpardo@gmail.com>
2024-03-19 23:43:25 -0500

    Animated scrren p-1

--- lib/presentation/screens/animated/animated_screen.dart
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -6,6 +6,24 @@ class AnimatedScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Animated Container'),
+      ),
+      body: Center(
+        child: AnimatedContainer(
+          duration: const Duration(microseconds: 400),
+          curve: Curves.easeOutCubic,
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+              color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.play_arrow_rounded),
+      ),
+    );
   }
 }
