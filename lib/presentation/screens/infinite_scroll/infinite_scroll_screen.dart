47b4a5162dfc7d10d745027fdb75f732cde14252
Santiago Millan <santiagomillanpardo@gmail.com>
2024-03-22 01:20:42 -0500

    movimiento de scroll para indicar que se cargaron las nuevas img

--- lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -44,6 +44,7 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
     if (!isMounted) return;
     setState(() {});
     //todo
+    moveScrollToBottom();
   }
 
   Future<void> onRefresh() async {
@@ -60,6 +61,14 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
     addFiveImages();
 
     setState(() {});
+  }
+
+  void moveScrollToBottom() {
+    if (scrollController.position.pixels + 100 <=
+        scrollController.position.maxScrollExtent) return;
+    scrollController.animateTo(scrollController.position.pixels + 120,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.fastOutSlowIn);
   }
 
   void addFiveImages() {
