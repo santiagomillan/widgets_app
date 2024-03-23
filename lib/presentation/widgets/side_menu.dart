86398c63fc1058a7c39d74f42de390768433ef53
Santiago Millan <santiagomillanpardo@gmail.com>
2024-03-23 00:02:07 -0500

    Mostrar items del menu

--- lib/presentation/widgets/side_menu.dart
+++ lib/presentation/widgets/side_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class SideMenu extends StatefulWidget {
   const SideMenu({super.key});
@@ -11,6 +12,7 @@ class _SideMenuState extends State<SideMenu> {
   int navDrowerIndex = 0;
   @override
   Widget build(BuildContext context) {
+    final hasNotch = MediaQuery.of(context).viewPadding.top > 35;
     return NavigationDrawer(
         selectedIndex: navDrowerIndex,
         onDestinationSelected: (value) {
@@ -18,12 +20,27 @@ class _SideMenuState extends State<SideMenu> {
             navDrowerIndex = value;
           });
         },
-        children: const [
-          NavigationDrawerDestination(
-              icon: Icon(Icons.add), label: Text('Home Screen')),
-          NavigationDrawerDestination(
-              icon: Icon(Icons.add_shopping_cart_outlined),
-              label: Text('Other Screen'))
+        children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(28, hasNotch ? 10 : 20, 16, 10),
+            child: const Text('Main'),
+          ),
+          ...appMenuItems.sublist(0, 3).map(
+                (item) => NavigationDrawerDestination(
+                    icon: Icon(item.icon), label: Text(item.title)),
+              ),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(28, 16, 28, 10),
+            child: Divider(),
+          ),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(28, 10, 16, 10),
+            child: Text('More Options'),
+          ),
+          ...appMenuItems.sublist(3).map(
+                (item) => NavigationDrawerDestination(
+                    icon: Icon(item.icon), label: Text(item.title)),
+              ),
         ]);
   }
 }
